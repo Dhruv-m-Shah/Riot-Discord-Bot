7f9902624c93c6b5fb5fc604a4569d51fe6067b1
Dhruv Shah <42727015+Dhruv-m-Shah@users.noreply.github.com>
2020-06-06 16:16:47 -0400

    bot status

--- js/main.js
+++ js/main.js
@@ -25,9 +25,6 @@ bot.on('ready', () => {
   bot.user.setActivity('.help', { type: 'WATCHING' })
 })
 
-
-//
-client.user.setActivity(""); 
 const http = require("http");
 var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
