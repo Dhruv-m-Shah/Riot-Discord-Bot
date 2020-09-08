3b118058c412ba4db65e060ed5d2d42d7beb5918
Dhruv Shah <42727015+Dhruv-m-Shah@users.noreply.github.com>
2020-09-07 23:06:21 -0400

    Added remove from data base command

--- js/db.js
+++ js/db.js
@@ -18,13 +18,20 @@ async function dbConnect() {
 }
 
 async function insert_in_database(champName, channelId, serverId) {
-    const mainFunctions = require("./main");
+    const mainFunctions = require("./main"); // refactor code so this only needs to be inserted once.
     let playerExists = await mainFunctions.get_player_id(champName, channelId, "insert");
-    console.log(playerExists);
+    if(playerExists == -1){
+        // message sent in get_player_id function in main.js
+        return;
+    }
     col = await client.db("gromp").collection("grompUsers");
-
     var cursor = await col.findOne( {_id: serverId});
-    console.log(cursor);
+    for(let i = 0; i < cursor.champNames.length; i++){
+        if(cursor.champNames[i] == champName){
+            mainFunctions.bot.channels.cache.get(channelId).send("That summoner is already registered!");
+            return;
+        }
+    }
   await col.updateOne(
     { _id: serverId },
     {
@@ -32,9 +39,30 @@ async function insert_in_database(champName, channelId, serverId) {
     },
     { upsert: true }
   );
+  mainFunctions.bot.channels.cache.get(channelId).send(champName + " has been registered!");
+}
+
+async function delete_from_database(champName, channelId, serverId){
+    const mainFunctions = require("./main"); // refactor code so this only needs to be inserted once.
+    col = await client.db("gromp").collection("grompUsers");
+    var cursor = await col.findOne( {_id: serverId});
+    if(!cursor.champNames.includes(champName)){
+        mainFunctions.bot.channels.cache.get(channelId).send("That summoner has not been registered yet!");
+    }
+    else{
+        await col.updateOne(
+            { _id: serverId },
+            {
+              $pull: { champNames: champName },
+            },
+            { upsert: true }
+          );
+        mainFunctions.bot.channels.cache.get(channelId).send(champName + " has been removed!");
+    }
 }
 
 module.exports = {
   dbConnect,
   insert_in_database,
+  delete_from_database
 };
